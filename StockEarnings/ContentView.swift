bb347c4e23d9de9473102bd00dd68c5e1ce5bd2e
Yashas Rai <yashasrai@gmail.com>
2026-08-26 17:47:40 -0700

    First commit

---
+++ StockEarnings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockEarnings
+//
+//  Created by Yashas Rai on 8/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockEarnings/StockEarningsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockEarningsApp.swift
+//  StockEarnings
+//
+//  Created by Yashas Rai on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct StockEarningsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
